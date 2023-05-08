8c175f767538f7f83ca4d235a3eee259f552df68
JustHm <taggma12@gmail.com>
2023-05-08 21:16:53 +0900

    Initial Commit

---
+++ TripGroup/TripGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripGroupApp.swift
+//  TripGroup
+//
+//  Created by 안정흠 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct TripGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
